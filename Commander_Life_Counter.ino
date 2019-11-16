9411d020d2ed3219e58b3bd62e0f8cd3327952cf
smokeandlights <smokeandlights@hotmail.com>
2019-11-16 10:09:42 -0500

    Add files via upload

--- Commander_Life_Counter.ino
+++ Commander_Life_Counter.ino
@@ -1,62 +1,64 @@
 #include <BigNumbers.h> //https://github.com/seanauff/BigNumbers
-#include <DFRkeypad.h>  //https://github.com/DFRobot/LCD-KeyPad-Shield
 
-#define DEBUG
-
+//#define DEBUG
+//#define AUTOSCROLL
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 BigNumbers bigNum(&lcd);
 
 int LifeTtl = 40;
 
-String Cmdr0LabelLine1 = "Scott";
+String Cmdr0LabelLine1 = "Plyr 1";
 String Cmdr0LabelLine2 = "CMDR DMG";
 int Cmdr0 = 0;
 
-String Cmdr1LabelLine1 = "Harber";
+String Cmdr1LabelLine1 = "Plyr 2";
 String Cmdr1LabelLine2 = "CMDR DMG";
-int Cmdr1 = 1;
+int Cmdr1 = 0;
 
-String Cmdr2LabelLine1 = "Barnett";
+String Cmdr2LabelLine1 = "Plyr 3";
 String Cmdr2LabelLine2 = "CMDR DMG";
-int Cmdr2 = 2;
+int Cmdr2 = 0;
 
-String Cmdr3LabelLine1 = "Doc";
+String Cmdr3LabelLine1 = "Plyr 4";
 String Cmdr3LabelLine2 = "CMDR DMG";
-int Cmdr3 = 3;
+int Cmdr3 = 0;
 
-String Ctr1LabelLine1 = "Counter";
-String Ctr1LabelLine2 = "   1";
-int Ctr1 = 4;
+String Ctr1LabelLine1 = "Missed";
+String Ctr1LabelLine2 = "Land Drops";
+int Ctr1 = 0;
 
 String Ctr2LabelLine1 = "Counter";
-String Ctr2LabelLine2 = "   2";
-int Ctr2 = 5;
+String Ctr2LabelLine2 = "   1";
+int Ctr2 = 0;
 
 String Ctr3LabelLine1 = "Treasure";
 String Ctr3LabelLine2 = " Tokens";
-int Ctr3 = 6;
+int Ctr3 = 0;
 
 String Ctr4LabelLine1 = " Poison";
 String Ctr4LabelLine2 = "Counters";
-int Ctr4 = 7;
+int Ctr4 = 0;
 
-int modeVar[9] {LifeTtl, Cmdr0, Cmdr1, Cmdr2, Cmdr3, Ctr1, Ctr2, Ctr3, Ctr4};
-int modeBigNumPos[9] {9, 10, 10, 10, 10, 9, 9, 9, 11};
-int modeBigNumPlaces[9] {3, 2, 2, 2, 2, 3, 3, 3, 2};
-String modeLabelLine1[9] {"Life", Cmdr0LabelLine1, Cmdr1LabelLine1, Cmdr2LabelLine1, Cmdr3LabelLine1, Ctr1LabelLine1, Ctr2LabelLine1, Ctr3LabelLine1, Ctr4LabelLine1};
-String modeLabelLine2[9] {"Total", Cmdr0LabelLine2, Cmdr1LabelLine2, Cmdr2LabelLine2, Cmdr3LabelLine2, Ctr1LabelLine2, Ctr2LabelLine2, Ctr3LabelLine2, Ctr4LabelLine2};
+String Ctr5LabelLine1 = "FUCKS";
+String Ctr5LabelLine2 = "GIVEN";
+int Ctr5 = 0;
 
+int modeVar[10] {LifeTtl, Cmdr0, Cmdr1, Cmdr2, Cmdr3, Ctr1, Ctr2, Ctr3, Ctr4, Ctr5};
+int modeBigNumPos[10] {6, 8, 8, 8, 8, 9, 8, 8, 8, 10}; //adjusts the placement of the big numbers on the display
+int modeBigNumPlaces[10] {3, 2, 2, 2, 2, 2, 2, 2, 2, 1}; //how many digits will be diplayed in the big numbers. Whole numbers only, no decimals, etc.
+String modeLabelLine1[10] {"Life", Cmdr0LabelLine1, Cmdr1LabelLine1, Cmdr2LabelLine1, Cmdr3LabelLine1, Ctr1LabelLine1, Ctr2LabelLine1, Ctr3LabelLine1, Ctr4LabelLine1, Ctr5LabelLine1};
+String modeLabelLine2[10] {"Total", Cmdr0LabelLine2, Cmdr1LabelLine2, Cmdr2LabelLine2, Cmdr3LabelLine2, Ctr1LabelLine2, Ctr2LabelLine2, Ctr3LabelLine2, Ctr4LabelLine2, Ctr5LabelLine2};
 
-byte scratch ;
 int mode = 0;
-
-bool buttonsWork = false; //change to true to disable auto page switching
+int increment = 1;
+String buttonPressed = "none";
 
 unsigned long previousMillis = 0;
 const long interval = 5000;
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(A0, INPUT);
   Serial.begin(9600);
   lcd.begin(16, 2);
   bigNum.begin();
@@ -65,35 +67,114 @@ void setup() {
 
 void loop() {
 #ifdef DEBUG
-  Serial.print(modeLabelLine1[mode]);
-  Serial.print(" ");
-  Serial.print(modeLabelLine2[mode]);
-  Serial.print(": ");
-  Serial.println(modeVar[mode]);
+
+  for (int m = 0; m <= 8; m++) {
+    Serial.print(modeLabelLine1[m]);
+    Serial.print(" ");
+    Serial.print(modeLabelLine2[m]);
+    Serial.print(": ");
+    Serial.print(modeVar[m]);
+    Serial.print("   ");
+  }
+  Serial.println();
 #endif
 
 
-  scratch = modeVar[mode];
   lcd.setCursor(0, 0);
   lcd.print(modeLabelLine1[mode]);
   lcd.setCursor(0, 1);
   lcd.print(modeLabelLine2[mode]);
-  bigNum.displayLargeInt(scratch, modeBigNumPos[mode], modeBigNumPlaces[mode], false);
-  modeVar[mode] = scratch;
+  if (increment == 10) {
+    lcd.setCursor(15, 0);
+    lcd.print("*");
+  }
+  if (increment == 1) {
+    lcd.setCursor(15, 0);
+    lcd.print(" ");
+  }
+  readButton();
+  bigNum.displayLargeInt(modeVar[mode], modeBigNumPos[mode], modeBigNumPlaces[mode], false);
 
 
-  //Auto mode switching until I figure out how to use buttons
-  if (buttonsWork == false) {
-    unsigned long currentMillis = millis();
-    if (currentMillis - previousMillis >= interval) {
-      previousMillis = currentMillis;
-      if (mode < 8) {
-        mode++;
-      }
-      else {
-        mode = 0;
-      }
+#ifdef AUTOSCROLL   //Auto mode switching until I figure out how to use buttons
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    if (mode < 9) {
+      mode++;
+      lcd.clear(); //blank the screen so there's no leftovers from the previous mode being displayed. Do this only when changing modes, or there's visible "scanning"
+    }
+    else {
+      mode = 0;
+      lcd.clear();//blank the screen so there's no leftovers from the previous mode being displayed. Do this only when changing modes, or there's visible "scanning"
+    }
+  }
+#endif
+
+}
+
+void readButton() { //hopefully, this will read the button pin, debounce, and change the buttonPressed variable for a brief time to change modes or change the variable value.
+
+  int b = analogRead(A0);
+  int i = 50;
+
+  Serial.print("Button Pressed: ");
+  Serial.print(buttonPressed);
+  Serial.print("   Analog reading: ");
+  Serial.print(analogRead(A0));
+  Serial.print("   Increment value: ");
+  Serial.println(increment);
+
+
+  if (b >= 1020) {
+    buttonPressed = "none";
+  }
+
+  if (b < 740 && b > 700) { //select should alternate the increment value between one and 10.
+    buttonPressed = "select";
+    if (increment == 1) {
+      increment = 10;
+      delay(i);
+    }
+    //    if (increment == 10) {
+    else {
+      increment = 1;
+      delay(i);
     }
   }
 
+  if (b < 325 && b > 285) {
+    buttonPressed = "down";
+    if (modeVar[mode] > 0) {
+      modeVar[mode] = modeVar[mode] - increment;
+      delay(i);
+    }
+  }
+
+  if (b < 150 && b > 110) {
+    buttonPressed = "up";
+    modeVar[mode] = modeVar[mode] + increment;
+    delay(i);
+  }
+
+  if (b < 500 && b > 460) { // left should change to the previous mode
+    buttonPressed = "left";
+    if (mode > 0) {
+      mode--;
+      lcd.clear();
+      delay(i);
+    }
+  }
+
+  if (b <= 10) {
+    buttonPressed = "right";
+    if (mode < 9) {
+      mode++;
+      lcd.clear();
+      delay(i);
+    }
+  }
+
+  //  }
+  delay(100);
 }
